5bfb11f281803acaf9e455121a85908b2a4354ff
Kane <yunhua.xiao@dianping.com>
2015-01-14 16:48:32 +0800

    url gevent

---
+++ gevent-url.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+__author__ = '__main__'
+
+from gevent import monkey; monkey.patch_all()
+import gevent
+import urllib2
+
+def f(url):
+    print 'GET: %s' % url
+    resp = urllib2.urlopen(url)
+    data = resp.read()
+    print '%d bytes received from %s' % (len(data), url)
+
+gevent.joinall([
+    gevent.spawn(f, 'https://www.python.org/'),
+    gevent.spawn(f, 'https://www.yahoo.com/'),
+    gevent.spawn(f, 'https://github.com')
+])
+
